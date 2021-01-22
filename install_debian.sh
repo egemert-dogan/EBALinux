75c1ac6bc995c143c73ad069d87f4f404ada70e1
egemertdogan <egemertdogan66@gmail.com>
2021-01-22 19:27:30 +0300

    Yükleme konumu değiştirildi ve hata düzenlemeleri yapıldı.

--- install_debian.sh
+++ install_debian.sh
@@ -2,7 +2,9 @@
  
 echo -e "\e[32mKurulum Başlıyor...\e[39m"
  
-cd ~/
+mkdir /opt
+
+cd opt
  
 echo -e "\e[32mSistem kaynaklarınız yenileniyor...\e[39m"
  
@@ -20,24 +22,26 @@ echo -e "\e[32mwget indiriliyor\e[39m"
  
 sudo apt-get install wget
  
-mkdir EBALinux
+sudo mkdir /opt/EBAlinux
  
-cd EBALinux
+cd /opt/EBALinux
 
 echo -e "\e[32mLogo indiriliyor...\e[39m"
 
-wget -O logo.png -P ~/EBALinux/ https://raw.githubusercontent.com/egemertdogan/EBALinux/main/logo.png
+sudo wget -O logo.png -P /opt/EBALinux/ https://raw.githubusercontent.com/egemertdogan/EBALinux/main/logo.png
 
 echo -e "\e[32mKullanıcı Betiği indiriliyor...\e[39m"
  
-wget -O user_Script.js -P ~/EBALinux/ https://raw.githubusercontent.com/caglarturali/eba_zoom_link/master/EBA_Zoom_Link.user.js
+sudo wget -O userScript.js -P /opt/EBALinux/ https://raw.githubusercontent.com/caglarturali/eba_zoom_link/master/EBA_Zoom_Link.user.js
 
 echo -e "\e[32mUygulama hazırlanıyor...\e[39m"
  
 sudo npm install -g nativefier
 
 echo -e "\e[32mUygulama yükleniyor...\e[39m"
- 
-nativefier "https://www.eba.gov.tr/#/anasayfa" --name EBALinux -i ~/EBALinux/logo.png --inject ~/EBALinux/user_Script.js
+
+cd /opt
+
+sudo nativefier "https://www.eba.gov.tr/#/anasayfa" --name EBALinux -i /opt/EBALinux/logo.png --inject /opt/EBALinux/user_Script.js
 
 echo -e "\e[32mTamamlandı!\e[39m"
