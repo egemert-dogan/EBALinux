8ba77f6c257c44b5af12cc90b6188f8b732ba0fe
egemertdogan <75202865+egemertdogan@users.noreply.github.com>
2021-01-23 11:20:21 +0300

    Update install_arch.sh

--- Scripts/install_arch.sh
+++ Scripts/install_arch.sh
@@ -2,42 +2,46 @@
  
 echo -e "\e[32mKurulum Başlıyor...\e[39m"
  
-cd ~/
+mkdir /opt
+
+cd /opt
  
 echo -e "\e[32mSistem kaynaklarınız yenileniyor...\e[39m"
  
-sudo pacman -Syy
+sudo apt-get update
  
 echo -e "\e[32mnpm indiriliyor\e[39m"
  
-sudo pacman -S npm
+sudo pacman -S npm --needed
 
 echo -e "\e[32mgit indiriliyor\e[39m"
  
-sudo pacman -S git
+sudo pacman -S git --needed
 
 echo -e "\e[32mwget indiriliyor\e[39m"
  
-sudo pacman -S wget
+sudo pacman -S wget --needed
  
-mkdir EBALinux
+sudo mkdir /opt/EBALinux
  
-cd EBALinux
+cd /opt/EBALinux
 
 echo -e "\e[32mLogo indiriliyor...\e[39m"
 
-wget -O logo.png -P ~/EBALinux/ https://raw.githubusercontent.com/egemertdogan/EBALinux/main/logo.png
+sudo wget -O logo.png -P /opt/EBALinux/ https://raw.githubusercontent.com/egemertdogan/EBALinux/main/logo.png
 
 echo -e "\e[32mKullanıcı Betiği indiriliyor...\e[39m"
  
-wget -O user_Script.js -P ~/EBALinux/ https://raw.githubusercontent.com/caglarturali/eba_zoom_link/master/EBA_Zoom_Link.user.js
+sudo wget -O userScript.js -P /opt/EBALinux/ https://raw.githubusercontent.com/caglarturali/eba_zoom_link/master/EBA_Zoom_Link.user.js
 
 echo -e "\e[32mUygulama hazırlanıyor...\e[39m"
  
 sudo npm install -g nativefier
 
 echo -e "\e[32mUygulama yükleniyor...\e[39m"
- 
-nativefier "https://www.eba.gov.tr/#/anasayfa" --name EBALinux -i ~/EBALinux/logo.png --inject ~/EBALinux/user_Script.js
+
+cd /opt
+
+sudo nativefier "https://www.eba.gov.tr/#/anasayfa" --name EBALinux -i /opt/EBALinux/logo.png --inject /opt/EBALinux/userScript.js
 
 echo -e "\e[32mTamamlandı!\e[39m"
