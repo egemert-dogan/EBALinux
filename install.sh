79c5563b5f8a4afa65de94b3879420e90c4d9908
egemertdogan <egemertdogan66@gmail.com>
2021-02-08 15:47:24 +0000

    Kurulum aşaması kolaylaştırıldı

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\e[32mEBALinux v1.1\e[39m"
+
+if [ -x "$(command -v pacman)" ]; 
+then sudo pacman -S wget npm git --needed ; cd /tmp/ ; sudo wget https://raw.githubusercontent.com/egemertdogan/EBALinux/main/Scripts/install_arch.sh ; bash install_arch.sh
+
+elif [ -x "$(command -v apt-get)" ];
+then sudo apt-get install wget npm git ; cd /tmp/ ; wget https://raw.githubusercontent.com/egemertdogan/EBALinux/main/Scripts/install_debian.sh ; bash install_debian.sh
+
+elif [ -x "$(command -v dnf)" ];
+then sudo dnf install wget nodejs git cd /tmp/ ; sudo wget https://raw.githubusercontent.com/egemertdogan/EBALinux/main/Scripts/install_rhel.sh ; bash install_rhel.sh
+
+elif [ -x "$(command -v xbps-install)" ];
+then sudo xbps-install wget nodejs git ; cd /tmp/ ; sudo wget https://raw.githubusercontent.com/egemertdogan/EBALinux/main/Scripts/install_void.sh ; bash install_void.sh
+
+else 
+	echo "Desteklenmeyen paket yönetim sistemi! Lütfen GitHub sayfasında bunu bildirin."
+
+fi
+
